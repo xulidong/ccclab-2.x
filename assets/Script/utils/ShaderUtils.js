ecb539b98c7a00ab97052ad644d6ef742975d6f1
xulidong <xu.lidong@qq.com>
2019-07-18 12:33:01 +0800

    fix spell error

---
+++ assets/Script/utils/ShaderUtils.js
@@ -0,0 +1,45 @@
+/** 
+ *  材质操作管理工具
+ */
+let MatUtils = {
+    customCache: {}, // 保存自定义材质
+};
+
+MatUtils.getMaterial = function(sp, name, isBuildIn, cb) {
+    if (isBuildIn) {
+        let mat = cc.Material.getInstantiatedBuiltinMaterial(name, sp);
+        cb(false, mat);
+    } else {
+        let url = "mat/" + name;
+        if (this.customCache[url]) {
+            cb(false, this.customCache[url]);
+            return;
+        }
+
+        cc.loader.loadRes(url, cc.Material, (err, asset) => {
+            if (err) {
+                cc.error(err);
+                cb(err, null);
+            }
+            this.customCache[url] = asset;
+            cb(false, asset);
+        });
+    }
+};
+
+MatUtils.useShader = function(sp, shaderName, args, cb) {
+    MatUtils.getMaterial(sp, shaderName, args.buildIn, (err, mat)=>{
+        if (err) {
+            cb(err, null);
+        };
+        mat.setProperty('texture', sp.spriteFrame.getTexture());
+        sp.setMaterial(0, mat);
+        sp.markForRender(true);
+
+        if (cb) {
+            cb(err, mat);
+        } 
+    });
+};
+
+module.exports = MatUtils;
